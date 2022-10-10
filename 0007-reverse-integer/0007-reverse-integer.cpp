6e60c1b8654ce26619ebe32676a6eea34f460c37
Viraj Raundal <72504150+VirajRaundal@users.noreply.github.com>
2022-10-10 23:59:42 +0530

    Time: 4 ms (46.55%), Space: 5.8 MB (95.35%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+        
+        if(x<=INT_MIN || x>=INT_MAX){
+            return 0;
+        }
+        
+        long long rev = 0;
+        if(x>0){
+            for(long long i = x; i>0; i/=10)
+                rev = rev*10 + i%10;
+        }
+        
+        else if(x<0){
+            for(long long i = -1 * x; i>0; i/=10)
+                rev = rev*10 + i%10;
+            
+            rev = rev*-1;
+        }
+        return (rev<INT_MIN || rev>INT_MAX) ? 0 : rev;
+    }
+};
