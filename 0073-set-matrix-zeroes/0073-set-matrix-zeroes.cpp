3be7af7a9bd20613ab1d5612b2a6578fa5f1e5c0
Viraj Raundal <72504150+VirajRaundal@users.noreply.github.com>
2022-10-11 23:32:56 +0530

    Time: 27 ms (52.19%), Space: 13.2 MB (81.85%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -5,33 +5,29 @@ public:
         int m = a[0].size();
         bool firstRow = false;  // do we need to set first row zero
         bool firstCol = false;  // do we need to ser first col zero
-         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-					if(a[i][j] == 0){   // store rows and cols state in first row and col
-                        if(i==0) firstRow = true;
-                        if(j==0) firstCol = true;
-                        a[i][0] = 0;
-                        a[0][j] = 0;
+        
+        //Setting up flags in the first row and column after checking the elements in the matrix.
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++)
+				if(a[i][j] == 0){   
+                    if(i==0) firstRow = true;// Setting the flag for the first row.
+                    if(j==0) firstCol = true;// Setting the flag for the first column.
+                    
+                    a[i][0] = 0; //Setting up the flag for row i.
+                    a[0][j] = 0; //Setting up the flag for column j.
                 }
-            }
-        }
-        // cout<<firstRow<<" "<<firstCol<<endl;
-         
-        for(int i=1; i<n; i++){
-            for(int j=1; j<m; j++){
-                if(a[i][0] == 0 || a[0][j] == 0){
+        
+        //Setting elements in the inner matrix as zeroes according to the flags setup in the first row and column.
+        for(int i=1; i<n; i++)
+            for(int j=1; j<m; j++)
+                if(a[i][0] == 0 || a[0][j] == 0)
                     a[i][j] = 0;
-                }
-            }
-        }
         
-        if(firstRow){
+        //Setting elements in first row and column as zero if required.
+        if(firstRow)
             for(int i=0;i<m;i++) a[0][i] = 0;
-        }
         
-        if(firstCol){
+        if(firstCol)
             for(int i=0;i<n;i++) a[i][0] = 0;
-        }
     }
 };  
