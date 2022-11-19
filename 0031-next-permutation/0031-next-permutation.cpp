0f183cd2caa4baf98d721f329637b0b50e8bcd42
Viraj Raundal <72504150+VirajRaundal@users.noreply.github.com>
2022-11-19 23:00:49 +0530

    Time: 7 ms (65.25%), Space: 11.9 MB (99.92%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -10,7 +10,7 @@ public:
         int idx = -1; 
         int n = nums.size();
         
-        for(int i = n-1; i>0; i--)
+        for(int i = n-1; i>0; i--)//
             if(nums[i]>nums[i-1]){
                 idx = i;
                 break;
