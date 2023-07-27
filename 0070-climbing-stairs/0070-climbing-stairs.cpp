abc4d20ca117c38b5745db0c8a966b524570f951
Viraj Raundal <72504150+VirajRaundal@users.noreply.github.com>
2023-07-27 23:55:50 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (72.40%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int i = 1;
-        int j = 2;
-        int fibo = 0;
+        int ans = 1;
+        int one = 1;
+        int two = 1;
         
-        switch(n){
-                case 1: return 1;
-                case 2: return 2;
-                case 3: return 3;
+        for(int i = n-2; i>=0; i--){
+            ans = one + two;
+            two = one;
+            one = ans;
         }
         
-        for(int a = 1; a<n-1 ; a++){
-            fibo = i+j;
-            i=j;
-            j = fibo;
-        }
-        
-        return fibo;
-        
-        
+        return ans;
     }
 };
